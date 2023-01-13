851e63b04f38df4ef6f31860a48e9de7f6e7b6df
YomiPounds <53308339+YomiPounds@users.noreply.github.com>
2023-01-13 15:22:39 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-071c33e7823c066b5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Poundsoo"
+  }
+}
