6ddce0d7f7327b3933d81dbeab052ba170ee3c8c
YomiPounds <53308339+YomiPounds@users.noreply.github.com>
2023-01-16 17:33:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,11 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "myec2a" {
   ami           = "ami-071c33e7823c066b5"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "Poundohabas"
+    Name = "yomiec2"
   }
 }
